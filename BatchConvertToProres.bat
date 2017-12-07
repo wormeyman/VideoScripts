1e57b75086fe027109cb22f8c7be16b8c35196fe
Eric J <wormeyman@gmail.com>
2017-12-06 16:01:30 -0800

    Update Batch Prores script with documentation

--- BatchConvertToProres.bat
+++ BatchConvertToProres.bat
@@ -1,2 +1,6 @@
-for %%a in ("*.MXF") do .\ffmpeg.exe -i "%%a" -vcodec prores -profile:v 3 "prores\%%~na.mov"
+REM Convert MP4 files into Prores
+REM Change the *.MP4 for whatever you need to change
+REM you may need to create the prores folder
+REM Eventually this will be a powershell script but for now it just works.
+for %%a in ("*.MP4") do .\ffmpeg\ffmpeg.exe -i "%%a" -vcodec prores -profile:v 3 ".\prores\%%~na.mov"
 pause
