4d8e5aa93438a26a3801c9081b2fc0d7ee328061
Eric J <wormeyman@gmail.com>
2018-01-10 08:28:37 -0800

    Updated for Stream Link fork

--- windowsLiveStreamer.ps1
+++ windowsLiveStreamer.ps1
@@ -1,11 +1,12 @@
-#Updated 2017-11-01 13:38:04
-#livestreamer --verbose-player --player "'C:\Program Files (x86)\VideoLAN\VLC\vlc.exe' --file-caching=5000" --default-stream best THEURL
+#Updated 2018-01-10 08:23:50
+#streamlink --verbose-player --player "'C:\Program Files (x86)\VideoLAN\VLC\vlc.exe' --file-caching=5000" --default-stream best THEURL
 
 $video = Read-Host 'What is the video url?'
 # Update LiveStream
-Write-Host -ForegroundColor Green "Updating livestreamer"
-pip install --upgrade livestreamer
+Write-Host -ForegroundColor Green "Updating streamlink"
+pip install --upgrade streamlink
 Clear-Host
 # Watch the Stream
 Write-Host -ForegroundColor Green "Starting the Stream in VLC please wait while it loads"
-livestreamer --verbose-player --player "'C:\Program Files\VideoLAN\VLC\vlc.exe' --file-caching=5000" --default-stream best $video
+# streamlink --verbose-player --player "'C:\Program Files\VideoLAN\VLC\vlc.exe' --file-caching=5000" --default-stream best $video
+streamlink --verbose-player --player "C:\Program Files\VideoLAN\VLC\vlc.exe" --default-stream best $video
