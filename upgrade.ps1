1c040474b72cd7bf14e0946f1be013cca231f839
Eric J <wormeyman@gmail.com>
2018-02-19 15:01:59 -0800

    RIP Livestreamer, also switch to vlc 3

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,10 +1,10 @@
-# Upgrade youtube_dl and livestreamer in one command.
+# Upgrade youtube_dl and streamlink in one command.
 Write-Host -ForegroundColor Green "Updating Python pip"
 pip install --upgrade pip
 Write-Host -ForegroundColor Green "Updating YouTube-DL Python Edition"
 pip install --upgrade youtube_dl
-Write-Host -ForegroundColor Green "Updating livestreamer"
-pip install --upgrade livestreamer
+Write-Host -ForegroundColor Green "Updating streamlink"
+pip install --upgrade streamlink
 Write-Host -ForegroundColor Green "Updating YouTube-DL in Videos Folder"
 .\youtube-dl.exe -U
 Start-Sleep -s 2
--- windowsLiveStreamer.ps1
+++ windowsLiveStreamer.ps1
@@ -2,12 +2,12 @@
 #streamlink --verbose-player --player "'C:\Program Files (x86)\VideoLAN\VLC\vlc.exe' --file-caching=5000" --default-stream best THEURL
 
 $video = Read-Host 'What is the video url?'
-# Update LiveStream
+# Update streamlink
 Write-Host -ForegroundColor Green "Updating streamlink"
 pip install --upgrade streamlink
 Clear-Host
 # Watch the Stream
 Write-Host -ForegroundColor Green "Starting the Stream in VLC please wait while it loads"
 # streamlink --verbose-player --player "'C:\Program Files\VideoLAN\VLC\vlc.exe' --file-caching=5000" --default-stream best $video
-# streamlink --verbose-player --player "C:\Program Files\VideoLAN\VLC\vlc.exe" --default-stream best $video
-streamlink --verbose-player --player "C:\Program Files\MPC-HC\mpc-hc64.exe" --default-stream best $video
+streamlink --verbose-player --player "C:\Program Files\VideoLAN\VLC\vlc.exe" --default-stream best $video
+#streamlink --verbose-player --player "C:\Program Files\MPC-HC\mpc-hc64.exe" --default-stream best $video
