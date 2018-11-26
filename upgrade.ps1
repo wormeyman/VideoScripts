e886a219e7667c46e757e77ec5dbb27fab605bc3
Eric J <wormeyman@gmail.com>
2018-11-26 11:01:02 -0800

    update python updates and remove local update

--- upgrade.ps1
+++ upgrade.ps1
@@ -1,11 +1,12 @@
 # Upgrade youtube_dl and streamlink in one command.
 Write-Host -ForegroundColor Green "Updating Python pip"
-pip install --upgrade pip
+#pip install --upgrade pip
+python -m pip install --upgrade pip
 Write-Host -ForegroundColor Green "Updating YouTube-DL Python Edition"
 pip install --upgrade youtube_dl
 Write-Host -ForegroundColor Green "Updating streamlink"
 pip install --upgrade streamlink
-Write-Host -ForegroundColor Green "Updating YouTube-DL in Videos Folder"
-.\youtube-dl.exe -U
+#Write-Host -ForegroundColor Green "Updating YouTube-DL in Videos Folder"
+#.\youtube-dl.exe --update
 Start-Sleep -s 2
 Clear-Host
