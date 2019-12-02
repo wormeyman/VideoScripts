d80c04f421f5180ca46e04b54f1498901d913900
Eric J <wormeyman@gmail.com>
2019-12-01 16:35:33 -0800

    Forgot a space

--- youTubeDLPromptBash.sh
+++ youTubeDLPromptBash.sh
@@ -5,5 +5,5 @@ read -p "What is the video url?  " video
 echo "Updating YouTube-DL before Downloading"
 pip install --upgrade pip youtube-dl ffmpeg
 echo "Downloading Video"
-youtube-dl -o "~/storage/downloads/%(title)s.%(ext)s"--restrict-filenames $video
+youtube-dl -o "~/storage/downloads/%(title)s.%(ext)s" --restrict-filenames $video
 echo "The Script has Finished!"
