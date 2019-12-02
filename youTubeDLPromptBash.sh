8d46b04d9f1ba9a5109906844383e784e8b921bc
Eric J <wormeyman@gmail.com>
2019-12-01 16:17:36 -0800

    change location to downloads dir

--- youTubeDLPromptBash.sh
+++ youTubeDLPromptBash.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 # Updated: 2019-12-01 16:05:05
+# This script is created primarily for using with Termux on Andriod
 read -p "What is the video url?  " video
 echo "Updating YouTube-DL before Downloading"
 pip install --upgrade pip youtube-dl ffmpeg
+echo "Changing to downloads directory"
+cd storage/downloads/
+echo "Downloading Video"
 youtube-dl --restrict-filenames $video
 echo "The Script has Finished!"
