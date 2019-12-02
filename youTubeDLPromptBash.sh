e20e19ae8d512c10018718df62fb5482df1997a5
Eric J <wormeyman@gmail.com>
2019-12-01 16:33:03 -0800

    Use youtubedl to change output

--- youTubeDLPromptBash.sh
+++ youTubeDLPromptBash.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 # Updated: 2019-12-01 16:05:05
 # This script is created primarily for using with Termux on Andriod
-alias youTubeDLPromptDIR="cd storage/downloads/"
-# alias proj="cd /home/tree/projects/java"
 read -p "What is the video url?  " video
 echo "Updating YouTube-DL before Downloading"
 pip install --upgrade pip youtube-dl ffmpeg
-echo "Changing to downloads directory"
-$youTubeDLPromptDIR
 echo "Downloading Video"
-youtube-dl --restrict-filenames $video
+youtube-dl -o "~/storage/downloads/%(title)s.%(ext)s"--restrict-filenames $video
 echo "The Script has Finished!"
