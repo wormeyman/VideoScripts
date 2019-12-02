08299611b81ab4ba7ef1083961bb1d5a30240ae9
Eric J <wormeyman@gmail.com>
2019-12-01 16:23:27 -0800

    can't cd in a shell script

--- youTubeDLPromptBash.sh
+++ youTubeDLPromptBash.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 # Updated: 2019-12-01 16:05:05
 # This script is created primarily for using with Termux on Andriod
+alias youTubeDLPromptDIR="cd storage/downloads/"
+# alias proj="cd /home/tree/projects/java"
 read -p "What is the video url?  " video
 echo "Updating YouTube-DL before Downloading"
 pip install --upgrade pip youtube-dl ffmpeg
 echo "Changing to downloads directory"
-cd storage/downloads/
+$youTubeDLPromptDIR
 echo "Downloading Video"
 youtube-dl --restrict-filenames $video
 echo "The Script has Finished!"
