edc8159128bf783f4998dc51ebf4cc166e054355
Eric J <wormeyman@gmail.com>
2019-12-04 09:54:44 -0800

    Update youTubeDLPromptBash.sh

--- youTubeDLPromptBash.sh
+++ youTubeDLPromptBash.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-# Updated: 2019-12-01 16:05:05
+# Updated: 2019-12-04 09:54:01
 # This script is created primarily for using with Termux on Andriod
 read -p "What is the video url?  " video
 echo "Updating YouTube-DL before Downloading"
 pip install --upgrade pip youtube-dl ffmpeg
 echo "Downloading Video"
-# youtube-dl -o "~/storage/downloads/%(title)s.%(ext)s" --restrict-filenames
 youtube-dl -o '~/storage/downloads/%(title)s-%(id)s.%(ext)s' $video --restrict-filenames
 echo "The Script has Finished!"
